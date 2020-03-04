1cd59c9fa0297ee80f94867f0c43ddb437a60761
reibitto <reibitto@users.noreply.github.com>
2020-03-05 00:02:49 +0900

    Try with nonFlaky

--- src/test/scala/zio/process/ZIOProcessBaseSpec.scala
+++ src/test/scala/zio/process/ZIOProcessBaseSpec.scala
@@ -4,5 +4,5 @@ import zio.duration._
 import zio.test._
 
 trait ZIOProcessBaseSpec extends DefaultRunnableSpec {
-  override def aspects = List(TestAspect.timeout(5.seconds))
+  override def aspects = List(TestAspect.timeout(5.seconds), TestAspect.nonFlaky)
 }

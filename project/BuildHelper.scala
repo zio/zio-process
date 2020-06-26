613a2ba9bd67808365332f86573e4494d9cd3186
reibitto <reibitto@users.noreply.github.com>
2020-06-26 13:23:47 +0900

    Silencer plugin needs 2.12.11

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -5,7 +5,7 @@ import BuildInfoKeys._
 
 object BuildHelper {
   private val Scala211        = "2.11.12"
-  private val Scala212        = "2.12.10"
+  private val Scala212        = "2.12.11"
   private val Scala213        = "2.13.2"
   private val SilencerVersion = "1.7.0"
 

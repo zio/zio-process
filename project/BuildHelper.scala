a09dc440c39d3ac2b64178c378da1fb1c96eda12
Scala Steward <me@scala-steward.org>
2021-02-27 03:16:16 +0100

    Update scala-library to 2.13.5

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,7 +4,7 @@ import sbtbuildinfo._
 import BuildInfoKeys._
 
 object BuildHelper {
-  private val Scala211        = "2.11.12"
+  private val Scala211        = "2.13.5"
   private val Scala212        = "2.12.11"
   private val Scala213        = "2.13.3"
   private val SilencerVersion = "1.7.1"

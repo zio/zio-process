3c34f6bbcfddcdacfbc9703724226970d8b6fa6d
Scala Steward <me@scala-steward.org>
2021-05-17 15:30:00 +0200

    Update scala-library to 2.13.6

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import BuildInfoKeys._
 object BuildHelper {
   private val Scala211        = "2.11.12"
   private val Scala212        = "2.12.11"
-  private val Scala213        = "2.13.3"
+  private val Scala213        = "2.13.6"
   private val SilencerVersion = "1.7.1"
 
   private val stdOptions = Seq(

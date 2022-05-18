5d92f353f5be4c8443ee2791161e543655320d13
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-18 16:18:55 +0200

    Fix

--- src/main/scala/zio/process/ProcessStream.scala
+++ src/main/scala/zio/process/ProcessStream.scala
@@ -57,7 +57,7 @@ final case class ProcessStream(private val inputStream: InputStream) {
   def linesStream: ZStream[Any, CommandError, String] =
     stream
       .via(
-        ZPipeline.fromChannel(ZPipeline.utf8Decode.channel.mapError(CommandError.IOError))
+        ZPipeline.fromChannel(ZPipeline.utf8Decode.channel.mapError(CommandError.IOError.apply))
       )
       .via(ZPipeline.splitLines)
 

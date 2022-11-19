3ebb085e31416b2182b72ec961d59b45fb70a27e
Milad Khajavi <khajavi@gmail.com>
2022-11-19 14:20:37 +0330

    prepare docs.

---
+++ docs/sidebars.js
@@ -0,0 +1,12 @@
+const sidebars = {
+  sidebar: [
+    "index",
+    "getting-started",
+    "basics",
+    "piping",
+    "interactive-processes",
+    "other"
+  ]
+};
+
+module.exports = sidebars;

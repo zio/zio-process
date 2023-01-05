6db19128ab9e732325c4d7963df478420d188af8
Milad Khajavi <khajavi@gmail.com>
2023-01-05 19:01:41 +0330

    Move Introduction to ZIO Process Project (#322)
    * move introduction to the zio-process project.
    
    * remove extra index page.

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,11 +1,18 @@
 const sidebars = {
   sidebar: [
-    "index",
-    "getting-started",
-    "basics",
-    "piping",
-    "interactive-processes",
-    "other"
+    {
+      type: "category",
+      label: "ZIO Process",
+      collapsed: false,
+      link: { type: "doc", id: "index" },
+      items: [
+        "getting-started",
+        "basics",
+        "piping",
+        "interactive-processes",
+        "other"
+      ]
+    }
   ]
 };
 

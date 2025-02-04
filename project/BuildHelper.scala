78d088d43ea3f186eed13aa6e9201a9d8b11eb5d
Scala Steward <scala_steward@virtuslab.com>
2025-02-04 15:13:08 +0000

    Update scala3-library, ... to 3.3.5

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -8,7 +8,7 @@ object BuildHelper {
 
   val Scala212 = "2.12.19"
   val Scala213 = "2.13.14"
-  val Scala3   = "3.3.3"
+  val Scala3   = "3.3.5"
 
   private val stdOptions = Seq(
     "-encoding",

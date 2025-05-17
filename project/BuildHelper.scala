32934b2c3b80602115355bff13a6e61aa222200a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-05-17 23:48:19 +0200

    Update scala-library to 2.12.20 (#436)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import sbtcrossproject.CrossPlugin.autoImport._
 
 object BuildHelper {
 
-  val Scala212 = "2.12.19"
+  val Scala212 = "2.12.20"
   val Scala213 = "2.13.16"
   val Scala3   = "3.3.6"
 
